6e0b92976c560cbbda9553a2da79f1d443606e07
Bh Ma <blownhitherma@gmail.com>
2019-01-26 22:48:24 -0500

    [Feature] readout & description

--- js/speakout.js
+++ js/speakout.js
@@ -1,45 +1,50 @@
-let Sound = (function () {
-  var df = document.createDocumentFragment();
-  return function Sound(src) {
-    var snd = new Audio(src);
-    df.appendChild(snd); // keep in fragment until finished playing
-    snd.addEventListener('ended', function () { df.removeChild(snd); });
-    snd.play();
-    return snd;
-  }
-}());
+function readOut(text) {
 
-let xhr = new XMLHttpRequest();
-let url = 'https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=AIzaSyAGw8ciBzNPHdwzk3xtGePKp_SY4jtAAWA';
+    let Sound = (function () {
+        let df = document.createDocumentFragment();
+        return function Sound(src) {
+            let snd = new Audio(src);
+            df.appendChild(snd); // keep in fragment until finished playing
+            snd.addEventListener('ended', function () {
+                df.removeChild(snd);
+            });
+            snd.play();
+            return snd;
+        }
+    }());
 
-xhr.open("POST", url);
-var data = {
-  "input":
-    { "text": "Input Text" },
-  "audioConfig":
-  {
-    "audioEncoding": "LINEAR16",
-    "effectsProfileId": [
-      "handset-class-device"
-    ],
-    "pitch": "5.40",
-    "speakingRate": "0.95"
-  },
-  "voice":
-  {
-    "languageCode": "en-US",
-    "ssmlGender": "FEMALE",
-    "name": "en-US-Wavenet-C"
-  }
-};
+    let xhr = new XMLHttpRequest();
+    let url = 'https://texttospeech.googleapis.com/v1beta1/text:synthesize?key=AIzaSyAGw8ciBzNPHdwzk3xtGePKp_SY4jtAAWA';
 
-let audio = 'default';
-xhr.onreadystatechange = (e) => {
-  // audio = JSON.parse(xhr.responseText)['audioContent'];
-  let resp = JSON.parse(xhr.responseText);
-  resp = String(resp['audioContent']);
-  // console.log(resp);
-  let audio_visit = 'data:audio/wav;base64,' + resp;
-  var snd = Sound(audio_visit);
+    xhr.open("POST", url);
+    let data = {
+        "input":
+            {"text": text},
+        "audioConfig":
+            {
+                "audioEncoding": "LINEAR16",
+                "effectsProfileId": [
+                    "handset-class-device"
+                ],
+                "pitch": "5.40",
+                "speakingRate": "0.95"
+            },
+        "voice":
+            {
+                "languageCode": "en-US",
+                "ssmlGender": "FEMALE",
+                "name": "en-US-Wavenet-C"
+            }
+    };
+
+    let audio = 'default';
+    xhr.onreadystatechange = (e) => {
+        // audio = JSON.parse(xhr.responseText)['audioContent'];
+        let resp = JSON.parse(xhr.responseText);
+        resp = String(resp['audioContent']);
+        // console.log(resp);
+        let audio_visit = 'data:audio/wav;base64,' + resp;
+        let snd = Sound(audio_visit);
+    };
+    xhr.send(JSON.stringify(data));
 }
-xhr.send(JSON.stringify(data));
--- js/worddict.js
+++ js/worddict.js
@@ -1,19 +1,27 @@
-reply_map = {
-  "Mona Lisa": "A portrait painting by Leonardo da Vinci, the best known work of art in the world. Good for you!",
-  "Triangle": "Triangle is one of some basic gemetric shape, are you interested in Geometry or Math?",
-  "Cat": "What a lovely cat! Do you like dogs as well? Cats and dogs are best friends of human beings. ",
-  "Apple": "You drew an apple, is the apple your favorite fruit? Fruits are very helpful for your health.",
-  "Car": "Enjoy A day out at the fair? But vehicles might lead to air pollution, please try your best for environment protection.",
-  "Boat": "Row row row your boat, gently down the stream, oh the places you'll go, life is but a dream",
-  "Moon": "Starry, starry night Paint your palette blue and gray Look out on a summer's day",
-  "Bus": "the wheels on the bus goes round and round all through the town",
+const reply_map = {
+    "The Mona Lisa": "A portrait painting by Leonardo da Vinci, the best known work of art in the world. Good for you!",
+    "triangle": "Triangle is one of some basic gemetric shape, are you interested in Geometry or Math?",
+    "cat": "What a lovely cat! Do you like dogs as well? Cats and dogs are best friends of human beings. ",
+    "apple": "You drew an apple, is the apple your favorite fruit? Fruits are very helpful for your health.",
+    "car": "Enjoy A day out at the fair? But vehicles might lead to air pollution, please try your best for environment protection.",
+    "boat": "Row row row your boat, gently down the stream, oh the places you'll go, life is but a dream",
+    "moon": "Starry, starry night. Paint your palette blue and gray. Look out on a summer's day",
+    "bus": "the wheels on the bus goes round and round all through the town",
+};
+// let getpn = function(){
+//  // get your predicted noun
+// };
+// let pn = getpn();
+let random_pattern_reply = [
+    "I guess it's a {0}! Why you are interested in this?",
+    "Is it a {0}? Can you tell me more about your thinkings about {0}?",
+    "Wow, you showed me a {0}! Any good memories of this?",
+];
+
+function getDescription(keyword) {
+    if(keyword in reply_map) {
+        return  reply_map[keyword];
+    } else {
+        return random_pattern_reply[Math.floor(Math.random() * 3)].replace("{0}", keyword).replace("{0}", keyword);
+    }
 }
-let getpn = function(){
- // get your predicted noun 
-}
-let pn = getpn();
-random_pattern_reply = [
-  `I guess it's a ${pn}! Why you are interested in this?`,
-  `Is it a ${pn}? Can you tell me more about your thinkings about ${pn}?`,
-  `Wow, you showed me a ${pn}! Any good memories of this?`,
-]
