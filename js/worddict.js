d52918bc2bf19ec74e897bd478a07725967d7280
Bh Ma <blownhitherma@gmail.com>
2019-01-27 04:15:08 -0500

    [Feature] youtube demo embde

--- js/worddict.js
+++ js/worddict.js
@@ -1,11 +1,11 @@
 const reply_map = {
     "The Mona Lisa": "The Mona Lisa! A portrait painting by Leonardo da Vinci, the best known work of art in the world. Amazing!",
-    "triangle": "Triangle is one of some basic gemetric shape, are you interested in Geometry or Math?",
+    "triangle": "Triangle is one of some basic gemetric shape. Are you interested in Geometry or Math?",
     "cat": "What a lovely cat! Do you like dogs as well? Cats and dogs are best friends of human beings. ",
     "apple": "You drew an apple, is the apple your favorite fruit? Fruits are very helpful for your health.",
     "car": "Enjoy A day out at the fair? But vehicles might lead to air pollution, please try your best for environment protection.",
     "boat": "Row row row your boat, gently down the stream, oh the places you'll go, life is but a dream",
-    "moon": "Starry, starry night. Paint your palette blue and gray. Look out on a summer's day",
+    "moon": "Starry, starry night. Paint your palette blue and gray. Look out on a summer's day. ",
     "bus": "the wheels on the bus goes round and round all through the town",
 };
 // let getpn = function(){
