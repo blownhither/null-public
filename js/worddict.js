4e7c76f2acbff778d11ef39d1a56f38ac556e66d
AllenZYoung <allenzyoung97@gmail.com>
2019-01-26 22:14:52 -0500

    Update worddict.js

--- js/worddict.js
+++ js/worddict.js
@@ -2,6 +2,18 @@ reply_map = {
   "Mona Lisa": "A portrait painting by Leonardo da Vinci, the best known work of art in the world. Good for you!",
   "Triangle": "Triangle is one of some basic gemetric shape, are you interested in Geometry or Math?",
   "Cat": "What a lovely cat! Do you like dogs as well? Cats and dogs are best friends of human beings. ",
-  "Apple":"You drew an apple, is the apple your favorite fruit? Fruits are very helpful for your health.",
-  "Car": "Enjoy A day out at the fair? But vehicles might lead to air pollution, please try your best for environment protection."
+  "Apple": "You drew an apple, is the apple your favorite fruit? Fruits are very helpful for your health.",
+  "Car": "Enjoy A day out at the fair? But vehicles might lead to air pollution, please try your best for environment protection.",
+  "Boat": "Row row row your boat, gently down the stream, oh the places you'll go, life is but a dream",
+  "Moon": "Starry, starry night Paint your palette blue and gray Look out on a summer's day",
+  "Bus": "the wheels on the bus goes round and round all through the town",
 }
+let getpn = function(){
+ // get your predicted noun 
+}
+let pn = getpn();
+random_pattern_reply = [
+  `I guess it's a ${pn}! Why you are interested in this?`,
+  `Is it a ${pn}? Can you tell me more about your thinkings about ${pn}?`,
+  `Wow, you showed me a ${pn}! Any good memories of this?`,
+]
